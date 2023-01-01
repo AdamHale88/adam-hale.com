cebf6b13c72d0f3bc6b43f149d2da58e15be62be
adamhale88 <adamhale88@tuta.io>
2023-01-01 13:49:14 -0500

    fixed devloper enviorment build issues with iframe not having a title in resume iframe

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,8 +1,10 @@
+
 const Resume = () => {
     return (
         <>
             <div className="uk-container">
                 <iframe
+                    title="Adams Resume"
                     src="images/Adam_Olsey_Resume.PDF"
                     width="100%"
                     height="800px"
